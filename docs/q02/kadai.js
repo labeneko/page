4c65711ae928441179d680a5afcfc4cd14d20c08
labeneko <labecat@gmail.com>
2022-11-19 16:21:32 +0900

    おすすめの修正2つ

--- docs/q02/kadai.js
+++ docs/q02/kadai.js
@@ -12,6 +12,12 @@ function message() {
       var hour = now.getHours();
       var min = now.getMinutes();
       var sec = now.getSeconds();
+
+      mon = ( '00' + mon ).slice( -2 );
+      day = ( '00' + day ).slice( -2 );
+      hour = ( '00' + hour ).slice( -2 );
+      min = ( '00' + min ).slice( -2 );
+      sec = ( '00' + sec ).slice( -2 );
   
       //出力用
       var s = year + "/" + mon + "/" + day + "/" + hour + ":" + min + ":" + sec; 
